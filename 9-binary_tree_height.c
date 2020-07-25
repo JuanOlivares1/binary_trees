ea0600e68d5e9ee65a767429adbbc6b110cdeaf4
Juan Olivares <jdop2000@gmail.com>
2020-07-25 00:15:21 +0000

    9 - height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: root node
+ * @func: function
+ *
+ * Return: size_t
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left = 0, right  = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right != NULL)
+		right = binary_tree_height(tree->right) + 1;
+	else
+		right = 0;
+	if (tree->left != NULL)
+		left = binary_tree_height(tree->left) + 1;
+	else
+		left = 0;
+	if (left < right)
+		return (right);
+	return (left);
+}
