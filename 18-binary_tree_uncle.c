5eb7abc8960cf3bf90cf1d62108bd40057cefa49
Juan Olivares <jdop2000@gmail.com>
2020-07-25 02:00:55 +0000

    18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - that finds the uncle of a node
+ * @node: node
+ *
+ * Return: sibling
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent != NULL)
+		return (binary_tree_sibling(node->parent));
+
+	return (NULL);
+}
+
+
+/**
+ * binary_tree_sibling - that finds the sibling of a node
+ * @node: node
+ *
+ * Return: sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent != NULL)
+	{
+		if (node->parent->right != NULL)
+		{
+			if (node->parent->right->n == node->n)
+				return (node->parent->left);
+			else
+				return (node->parent->right);
+		}
+	}
+	return (NULL);
+}
